40acaf644e7bc8e2e28939de4009a619073fe3db
Peter Phanouvong <peterphanouvong@gmail.com>
2024-07-30 14:23:49 +1000

    trying stuff

--- src/app/api/auth/[...kindeAuth]/route.ts
+++ src/app/api/auth/[...kindeAuth]/route.ts
@@ -1,3 +1,5 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-export const GET = handleAuth();
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+export const GET = handleAuth();
