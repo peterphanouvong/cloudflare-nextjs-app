91cf423d2eb259f12022554eb94216605eb85a67
Peter Phanouvong <peterphanouvong@gmail.com>
2024-07-30 13:53:10 +1000

    edge

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,4 +1,2 @@
-import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server"
-export const GET = handleAuth()
-
-export const runtime = "edge"
+import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+export const GET = handleAuth();
